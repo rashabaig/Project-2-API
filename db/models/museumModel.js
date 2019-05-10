c6363f7fd9d825b2be260fdfceb62392184b8dfd
Rasha Baig <rasha.baig@gmail.com>
2019-05-10 15:47:59 -0400

    reformatted

--- db/models/museumModel.js
+++ db/models/museumModel.js
@@ -1,27 +1,27 @@
-const mongoose = require('../../connection')
+const mongoose = require("../../connection");
 
 const ExhibitModel = new mongoose.Schema({
-    exhibitName: String,
-    description: String
-})
+  exhibitName: String,
+  description: String
+});
 const ReviewModel = new mongoose.Schema({
-      stars: Number,
-      comment: String
-})
+  stars: Number,
+  comment: String
+});
 
 const MuseumModel = new mongoose.Schema({
-    name: String,
-    address: {
-      street: String,
-      city: String,
-      state: String,
-      zipcode: Number
-    },
-    metroAccessible: Boolean,
-    website: String,
-    freeAdmission: Boolean,
-    review: [ReviewModel], 
-    exhibit: [ExhibitModel]
-})
+  name: String,
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipcode: Number
+  },
+  metroAccessible: Boolean,
+  website: String,
+  freeAdmission: Boolean,
+  review: [ReviewModel],
+  exhibit: [ExhibitModel]
+});
 
-module.exports = mongoose.model("Museums", MuseumModel )
+module.exports = mongoose.model("Museums", MuseumModel);
--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const express = require('express')
-const app = express()
-const parser = require('body-parser')
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
 
-app.use(parser.urlencoded({extended: true}))
-app.use(parser.json())
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
 
-const museumController = require('./controllers/museums')
+const museumController = require("./controllers/museums");
 
-app.use('/api/museums/', museumController)
-app.use(express.static(__dirname + '/public'));
+app.use("/api/museums/", museumController);
+app.use(express.static(__dirname + "/public"));
 
-app.set('views', './views')
-app.set('view engine', 'pug')
+app.set("views", "./views");
+app.set("view engine", "pug");
 
-app.set("port", process.env.PORT || 3001)
-
+app.set("port", process.env.PORT || 3001);
 
 app.listen(app.get("port"), () => {
-  console.log(`✅ PORT: ${app.get("port")} 🌟`)
-})
-
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
